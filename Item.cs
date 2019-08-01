cdc71a775640caae7a45523dd33ec9bc42cd8b16
riang5924 <50815470+riang5924@users.noreply.github.com>
2019-08-01 13:04:56 +0900

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
